0ec9d81215596f9ad45b83387d499327ba6366a6
Rbashirov <bashirov.rk@gmail.com>
2018-05-20 22:29:08 +0300

    solution big

--- knapsack_problem_big.py
+++ knapsack_problem_big.py
@@ -6,16 +6,16 @@ for i in range(T):
      tmp = list(map(int,input().split()))
      v[i] = tmp[0]
      w[i] = tmp[1]
-#     print(w[i])
-for i in range(1,T+1):
-#    print(i)
-    for x in range(W+1):
 
+for i in range(1,T+1):
+    count=0
+    for x in range(W+1):
         if x-w[i-1] >= 0:
             A[1][x] = max(A[0][x], A[0][x-w[i-1]]+v[i-1])
+            count = 1
         else:
             A[1][x] = A[0][x]
-        for j in range(W+1):
-             A[0][j] = A[1][j]
-
-print(A[T][W])
+    if count == 1:
+        for j in range(len(A[0])):
+            A[0][j] = A[1][j]
+print(A[0][W])
